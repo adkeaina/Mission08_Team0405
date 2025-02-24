c00e8a5651a5dc5e60e8a7e95f84eea2ef8d6330
nicktimbellini <nicholas.bellini@live.com>
2025-02-24 14:08:20 -0700

    start

--- Mission08_Team0405/Controllers/HomeController.cs
+++ Mission08_Team0405/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ public class HomeController : Controller
     {
         return View();
     }
-
+    // THis is cool
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
