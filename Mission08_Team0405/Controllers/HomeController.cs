31879e5e267279234dfe291ff3499863ed190d4b
adkeaina <76181539+adkeaina@users.noreply.github.com>
2025-02-24 16:09:49 -0700

    stuffs

--- Mission08_Team0405/Controllers/HomeController.cs
+++ Mission08_Team0405/Controllers/HomeController.cs
@@ -27,9 +27,4 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
 }
--- Mission08_Team0405/Program.cs
+++ Mission08_Team0405/Program.cs
@@ -6,10 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// builder.Services.AddDbContext<GoalDbContext>(options =>
-// {
-//     options.UseSqlite(builder.Configuration.GetConnectionString("GoalConnection"));
-// });
+builder.Services.AddDbContext<GoalDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("GoalConnection"));
+});
 
 builder.Services.AddScoped<IGoalsRepository, EFGoalsRepository>();
 
